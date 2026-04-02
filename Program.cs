622580928aa97dbf8c3eaaa10626fd5966cd1088
EnyelJovier <enyelsantana1001@gmail.com>
2026-04-02 17:38:48 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Operaciones_Delegados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Diccionario con operaciones
+            Dictionary<int, Operaciones.Operacion> operaciones = new Dictionary<int, Operaciones.Operacion>()
+            {
+                {1, Operaciones.Sumar},
+                {2, Operaciones.Restar},
+                {3, Operaciones.Multiplicar},
+                {4, Operaciones.Dividir}
+            };
+
+            Console.WriteLine(" OPERACIONES CON DELEGADOS ");
+
+            Console.Write("Ingrese el primer numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nSeleccione el tipo de operacion:");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+
+            Console.Write("Opcion :");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            // Uso del delegado
+            Operaciones.Operacion op = operaciones[opcion];
+
+            double resultado = op(num1, num2);
+
+            Console.WriteLine("\nResultado: " + resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ operaciones.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Operaciones_Delegados
+{
+    public class Operaciones
+    {
+        // Delegado
+        public delegate double Operacion(double a, double b);
+
+        // Métodos
+        public static double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Dividir(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Error: no se puede dividir entre 0");
+                return 0;
+            }
+
+            return a / b;
+        }
+    }
+}
